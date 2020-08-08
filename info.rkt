912815769c43a4e7def5a154c1f664cbc91460e7
Cameron Moy <mail@camoy.name>
2020-08-08 19:20:23 -0400

    lib: Update info and build

--- info.rkt
+++ info.rkt
@@ -1,13 +1,22 @@
 #lang info
+
+;; General
+
 (define collection "redex")
-(define deps '("base" "redex-lib"))
+(define pkg-desc "Parameterized reduction relations in Redex.")
+(define version "0.0")
+(define pkg-authors '(camoy))
+(define scribblings '(("scribblings/redex-parameter.scrbl" ())))
+
+;; Dependencies
+
+(define deps
+  '("base"
+    "redex-lib"))
+
 (define build-deps
   '("scribble-lib"
     "racket-doc"
     "rackunit-lib"
     "redex-doc"
     "sandbox-lib"))
-(define scribblings '(("scribblings/redex-parameter.scrbl" ())))
-(define pkg-desc "Parameterized reduction relations in Redex.")
-(define version "0.0")
-(define pkg-authors '(camoy))
