6fbc0a7e122a81943262e54e7d3a601590a20bc6
Cameron Moy <mail@camoy.name>
2020-08-19 00:07:44 -0400

    lib: Fix scopes

--- parameter.rkt
+++ parameter.rkt
@@ -23,9 +23,7 @@
                      racket/sequence
                      racket/syntax
                      syntax/id-table
-                     syntax/parse
-                     syntax/strip-context
-                     racket/pretty)
+                     syntax/parse)
          racket/splicing
          redex/reduction-semantics)
 
@@ -63,7 +61,7 @@
     (define xs (syntax-local-value id (λ _ #f)))
     (when xs
       (match-define (list name params vals defn) xs)
-      (free-id-table-set! maker-table name (do-maker params vals defn))))
+      (free-id-table-set! maker-table name (do-maker name params vals defn))))
 
   ;; Identifier Identifier → Syntax
   ;; Returns the syntax needed to define the object `id` for `lang`.
@@ -132,16 +130,17 @@
 ;; generic parameterized syntax
 
 (begin-for-syntax
-  (define ((do-maker params vals defn) sc lang)
+  (define ((do-maker name params vals defn) sc lang)
     (define stx (make-params sc lang params vals))
     (with-syntax ([?lang (sc #'*LANG*)]
                   [([?param ?val ?lift] ...) stx])
-      #`(begin
-          ?lift ...
-          (splicing-let-syntax ([?lang (make-rename-transformer #'#,lang)]
-                                [?param (make-rename-transformer #'?val)]
-                                ...)
-            #,(sc defn)))))
+      (values (sc name)
+              #`(begin
+                  ?lift ...
+                  (splicing-let-syntax ([?lang (make-rename-transformer #'#,lang)]
+                                        [?param (make-rename-transformer #'?val)]
+                                        ...)
+                    #,(sc defn))))))
 
   ;; Identifier Identifier Syntax Syntax Syntax → Syntax
   ;; Returns syntax that defines the object (according to `defn`) parameterized
@@ -149,8 +148,9 @@
   (define (redex-obj-syntax name base lang params vals defn)
     (define mk-id (format-mk name))
     (define ext-id (format-ext name))
+    (define-values (_ stx) ((do-maker name params vals defn) values lang))
     #`(begin
-        #,((do-maker params vals defn) values lang)
+        #,stx
 
         (define-syntax #,mk-id
           (list #'#,name #'#,params #'#,vals #'((... ...) #,defn)))
@@ -183,10 +183,8 @@
   ;; Returns the syntax needed to lift the value to this language.
   (define (lift id lang)
     (define sc (make-syntax-introducer))
-    (define id* (sc id))
-    (define (sc* stx) (replace-context id* stx))
-    (define defn (apply-maker id sc* lang))
-    (list id* defn))
+    (define-values (defn-id defn) (apply-maker id sc lang))
+    (list defn-id defn))
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -286,109 +284,3 @@
                        #'(define-extended-judgment-form
                            *LANG* ?base* #:mode ?m ?more ...))
      #`(begin ?defn-base ?defn-form)]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; test
-
-(module+ test
-  (require chk
-           racket/set)
-
-  ;;
-  ;; Languages
-  ;;
-
-  (define-language L0
-    [m ::= number])
-
-  (define-language L1
-    [m ::= number string])
-
-  ;;
-  ;; Reduction Relations
-  ;;
-
-  (define-metafunction* L0
-    [(foo-mf0 m) 0])
-
-  (define-judgment-form* L0
-    #:mode (foo-jf0 I O)
-    [(foo-jf0 m 0)])
-
-  (define-reduction-relation* r0-mf
-    L0
-    #:parameters ([foo-mf foo-mf0])
-    [--> m (foo-mf m)])
-
-  (define-reduction-relation* r0-jf
-    L0
-    #:parameters ([foo-jf foo-jf0])
-    [--> m_1 m_2 (judgment-holds (foo-jf m_1 m_2))])
-
-  (define-reduction-relation* r0-rr
-    L0
-    #:parameters ([r r0-mf])
-    [--> m_1 m_2
-         (where (_ ... m_2 _ ...)
-                ,(apply-reduction-relation r (term m_1)))])
-
-  (chk
-   (apply-reduction-relation r0-mf (term 42)) '(0)
-   (apply-reduction-relation r0-jf (term 42)) '(0)
-   (apply-reduction-relation r0-rr (term 42)) '(0)
-   )
-
-  ;;
-  ;; Reduction Relation (lifted)
-  ;;
-
-  (define-extended-reduction-relation* r1-mf r0-mf L1)
-  (define-extended-reduction-relation* r1-jf r0-jf L1)
-  (define-extended-reduction-relation* r1-rr r0-rr L1)
-
-  (chk
-   (apply-reduction-relation r1-mf (term "foo")) '(0)
-   (apply-reduction-relation r1-jf (term "foo")) '(0)
-   (apply-reduction-relation r1-rr (term "foo")) '(0)
-   )
-
-  ;;
-  ;; Reduction Relation (extended)
-  ;;
-
-  (define-extended-metafunction* foo-mf0 L1
-    [(foo-mf1 m) 1.5])
-
-  (define-extended-judgment-form* foo-jf0 L1
-    #:mode (foo-jf1 I O)
-    [(foo-jf1 m 1.5)])
-
-  (define-extended-reduction-relation* r1.5-mf r0-mf L1)
-  (define-extended-reduction-relation* r1.5-jf r0-jf L1)
-  (define-extended-reduction-relation* r1.5-rr r0-rr L1)
-
-  (chk
-   (apply-reduction-relation r1.5-mf (term "foo")) '(1.5)
-   #:eq set=? (apply-reduction-relation r1.5-jf (term "foo")) '(0 1.5)
-   (apply-reduction-relation r1.5-rr (term "foo")) '(1.5)
-   )
-
-  ;;
-  ;; Judgment Form
-  ;;
-
-  (define-metafunction* L0
-    [(bar-mf0 m) 0])
-
-  (define-judgment-form* L0
-    #:parameters ([bar-mf bar-mf0])
-    #:mode (bar-jf0 I O)
-    [(bar-jf0 m (bar-mf m))])
-
-  (define-extended-judgment-form* bar-jf0 L1
-    #:mode (bar-jf1 I O))
-
-  (chk
-   #:t (judgment-holds (bar-jf0 0 0))
-   #:t (judgment-holds (bar-jf1 "bar" 0))
-   ))
